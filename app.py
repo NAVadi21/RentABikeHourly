94dd7f4af60eec810775b5eeee4be0a556b3fc63
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-05-28 20:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ ax.plot(train_data.head(30)['datetime'], train_data.head(30)['count'])
 plt.xticks(rotation='vertical')
 ax.set_xlabel('Date')
 ax.set_ylabel('Hourly bike rental demand')
-st.pyplot(fig)
+
 
 
 # extracting date, hour and month from the datetime
