4de545ac87855ab0cb372ea6f6324c5bb53e263b
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-06-21 01:07:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import java.util.*;
 import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-
 from sklearn.metrics import r2_score
  
 
